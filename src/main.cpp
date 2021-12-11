96e67a077652eddcd51f2e8d35e55e2e2b128e6f
Kevin Shen <88800700+Shen-Kev@users.noreply.github.com>
2021-12-10 22:41:06 -0800

    SD card output & MPU6050. Still needs fine details

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <Arduino.h>
+#include <Servo.h>
+#include <SPI.h>
+#include <SD.h>
+#include "Wire.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#include "I2Cdev.h"
 
-//PINS  //HIIIIIIIIIIIII
+File myFile;
+MPU6050 mpu;
+
+//PINS
+#define INTERRUPT_PIN 9
 #define S3PIN 2
 #define S4PIN 3
 #define S5PIN 4
@@ -15,7 +25,6 @@
 #define SERVO5PIN 15
 #define SERVO6PIN 14
 
-#include <Servo.h>
 Servo elevatorServo;
 Servo rotatorServo;
 Servo rightElevonServo;
@@ -60,6 +69,102 @@ bool isOptimum = true;
 
 float pitchDampener = 2;
 float elevonDampener = 2;
+
+// ================================================================
+// ===               MPU6050 STUFF- NOT MY WORK                 ===
+// ================================================================
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;        // [w, x, y, z]         quaternion container
+VectorInt16 aa;      // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;  // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld; // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity; // [x, y, z]            gravity vector
+float euler[3];      // [psi, theta, phi]    Euler angle container
+float ypr[3];        // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+// packet structure for InvenSense teapot demo
+uint8_t teapotPacket[14] = {'$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n'};
+
+//interrput detection
+volatile bool mpuInterrupt = false; // indicates whether MPU interrupt pin has gone high
+void dmpDataReady()
+{
+  mpuInterrupt = true;
+}
+
+void MPU6050Setup()
+{
+
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+  Wire.begin();
+  Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+  while (!Serial)
+    ; // wait for Leonardo enumeration, others continue immediately
+
+  // initialize device
+  Serial.println(F("Initializing I2C devices..."));
+  mpu.initialize();
+  pinMode(INTERRUPT_PIN, INPUT);
+
+  // verify connection
+  Serial.println(F("Testing device connections..."));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+  // load and configure the DMP
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+
+  // supply your own gyro offsets here, scaled for min sensitivity
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0)
+  {
+    // Calibration Time: generate offsets and calibrate our MPU6050
+    mpu.CalibrateAccel(6);
+    mpu.CalibrateGyro(6);
+    mpu.PrintActiveOffsets();
+    // turn on the DMP, now that it's ready
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
+
+    // enable Arduino interrupt detection
+    Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+    Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+    Serial.println(F(")..."));
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+
+    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
+
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  }
+  else
+  {
+    // ERROR!
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+    // (if it's going to break, usually the code will be 1)
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+  }
+}
+//MPU6050 STUFFS END
 
 //read pwm using interrupts
 volatile unsigned long PWMTimerStartStabilizedPitch;
@@ -161,7 +266,7 @@ void tailMovement()
 
   elevatorServoOutput = constrain(elevatorServoOutput + elevatorServoOutputTrim, 0, 180);
   rotatorServoOutput = constrain(rotatorServoOutput + rotatorServoOutputTrim, 0, 180);
-  rotatorServoOutput = 180-rotatorServoOutput;
+  rotatorServoOutput = 180 - rotatorServoOutput;
 }
 
 void justPitch()
@@ -172,7 +277,7 @@ void justPitch()
 void justElevons()
 {
   rightElevonServoOutput = ((stabilizedPitch + stabilizedRoll) / elevonDampener) + 90;
-  leftElevonServoOutput = (((0-stabilizedPitch) + stabilizedRoll) / elevonDampener) + 90;
+  leftElevonServoOutput = (((0 - stabilizedPitch) + stabilizedRoll) / elevonDampener) + 90;
 }
 
 void elevonWithTail()
@@ -197,7 +302,6 @@ void write()
   rightElevonServo.write(rightElevonServoOutput + rightElevonServoOutputTrim);
   leftElevonServo.write(leftElevonServoOutput + leftElevonServoOutputTrim);
 }
-
 void serialOutput()
 {
   Serial.print("  isOptimum: ");
@@ -217,7 +321,7 @@ void serialOutput()
   Serial.print("  right elevon: ");
   Serial.print(rightElevonServoOutput);
   Serial.print("  left elevon: ");
-  Serial.println(leftElevonServoOutput);
+  Serial.print(leftElevonServoOutput);
 }
 
 void spreadCalc()
@@ -230,8 +334,76 @@ void tailAdjustForSpread()
   //to be implemented
 }
 
+void SDSetup()
+{ //NOT MY WORK
+
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(10))
+  {
+    Serial.println("initialization failed!");
+    while (1)
+      ;
+  }
+  Serial.println("initialization done.");
+
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  myFile = SD.open("test.txt", FILE_WRITE);
+
+  // if the file opened okay, write to it:
+  if (myFile)
+  {
+    Serial.print("Writing to test.txt...");
+    myFile.println("testing 1, 2, 3.");
+    // close the file:
+    myFile.close();
+    Serial.println("done.");
+  }
+  else
+  {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+}
+
+void SDOutput()
+{
+
+  myFile = SD.open("test.txt", FILE_WRITE);
+
+  // if programming failed, don't try to do anything
+  if (!dmpReady)
+    return;
+
+  // read a packet from FIFO
+  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) //THIS IF STATEMENT MAKES IT SLOOOOOW BUT IT IS MIPORTANT FOR IT TOWORK
+  {                                            // Get the Latest packet
+    // display Euler angles in degrees
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    myFile.print("ypr\t");
+    myFile.print(ypr[0] * 180 / M_PI);
+    myFile.print("\t");
+    myFile.print(ypr[1] * 180 / M_PI);
+    myFile.print("\t");
+    myFile.println(ypr[2] * 180 / M_PI);
+
+    Serial.print("  ypr\t");
+    Serial.print(ypr[0] * 180 / M_PI);
+    Serial.print("\t");
+    Serial.print(ypr[1] * 180 / M_PI);
+    Serial.print("\t");
+    Serial.println(ypr[2] * 180 / M_PI);
+  }
+  myFile.close();
+}
+
 void setup()
 {
+  Serial.begin(115200);
+
   pinMode(stabilizedPitchInputPin, INPUT);
   pinMode(stabilizedYawInputPin, INPUT);
   pinMode(stabilizedRollInputPin, INPUT);
@@ -254,12 +426,14 @@ void setup()
   rightElevonServo.attach(rightElevonServoPin);
   leftElevonServo.attach(leftElevonServoPin);
 
-  //Serial.begin(115200);
+  SDSetup();
+  MPU6050Setup();
 }
 
 void loop()
 {
 
+  
   if (flyingWing)
   {
     justElevons();
@@ -283,6 +457,8 @@ void loop()
       elevonAsAileron();
     }
   }
-  write();
-  serialOutput();
+  
+  //write();
+  //serialOutput();
+  SDOutput();
 }
