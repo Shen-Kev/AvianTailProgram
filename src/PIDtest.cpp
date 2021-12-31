e45000b543be922e98d0e715474afc34c98d064a
Kevin Shen <88800700+Shen-Kev@users.noreply.github.com>
2021-12-30 21:07:06 -0800

    PID mostly done

---
+++ src/PIDtest.cpp
@@ -0,0 +1,38 @@
+const float PitchPgain = 1;
+const float PitchIgain = 0;
+const float PitchDgain = 1;
+
+float PitchProportional;
+float PitchIntegral;
+float PitchDerivative;
+float PitchError;
+float PrevPitchError;
+
+float PitchOutput;
+
+float RCpitch;//not new
+float pitch; //not new
+
+float prevPitch;
+float pitchChange;
+float pitchChangeMultiplier = 10;
+
+//need to add prevPitch right before pitch and calculate pitch change
+void PitchPID()
+{    
+
+    PrevPitchError = PitchError;
+    PitchError = RCpitch - pitchChange*pitchChangeMultiplier;
+
+
+    PitchProportional = PitchError * PitchPgain;
+
+    if(PitchIntegral <= 80 && PitchIntegral >= -80) {
+        PitchIntegral += PitchError * PitchIgain;
+    }
+
+    PitchDerivative = (PitchError-PrevPitchError) * PitchDgain;
+
+
+    PitchOutput = PitchProportional + PitchIntegral + PitchDerivative;
+}
